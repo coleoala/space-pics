8ae66f7c7c6f41efd56a5e31d55441ad221f726d
Cole Henson <robertcoleh@gmail.com>
2020-04-18 20:57:50 -0400

    Updates

---
+++ precache-manifest.de1b9913ffd7913a3d2f500739a19ac8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb298324b66dff4d3acd2bc1bca3d00e",
+    "url": "/coleoala/space-pics/index.html"
+  },
+  {
+    "revision": "58ef33a4feba56edf5c2",
+    "url": "/coleoala/space-pics/static/css/main.4278e55e.chunk.css"
+  },
+  {
+    "revision": "e03e4a600202288dce2e",
+    "url": "/coleoala/space-pics/static/js/2.ca31912d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coleoala/space-pics/static/js/2.ca31912d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58ef33a4feba56edf5c2",
+    "url": "/coleoala/space-pics/static/js/main.3340b556.chunk.js"
+  },
+  {
+    "revision": "59a33100d38dca36c37b",
+    "url": "/coleoala/space-pics/static/js/runtime-main.8a65cebc.js"
+  },
+  {
+    "revision": "b2f4c180386806e28d14532eccbe5247",
+    "url": "/coleoala/space-pics/static/media/photo.b2f4c180.svg"
+  },
+  {
+    "revision": "7990ca499b1853804cf51ce4a469ced4",
+    "url": "/coleoala/space-pics/static/media/refresh.7990ca49.svg"
+  },
+  {
+    "revision": "2d63439e97c74ec37092682918a861e1",
+    "url": "/coleoala/space-pics/static/media/stars.2d63439e.svg"
+  },
+  {
+    "revision": "12ff4969683e5ddd2ffe8967ed4267f7",
+    "url": "/coleoala/space-pics/static/media/video.12ff4969.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/coleoala/space-pics/precache-manifest.de1b9913ffd7913a3d2f500739a19ac8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/coleoala/space-pics/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
